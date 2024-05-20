1bcddbd829d08b37f6520a27e6edb716aea6903e
gabizinhzs <167816138+gabizinhzs@users.noreply.github.com>
2024-05-20 18:28:05 -0300

    Add files via upload

--- rotas.js
+++ rotas.js
@@ -1,4 +1,18 @@
-const express  = require('express') //npm i express --save
+const express  = require('express') 
 const app = express()
 
-const alunoDB = require('./app')
+const alunoDB = require('./app2')
+
+
+app.get('/usuarios', function(req,res){
+    res.type("usuarios.html")
+})
+
+
+
+//Iniciar servidor
+let serve = app.listen(3000, function(){
+    let host = server.address().address
+    let port = server.address().port
+    consoleog("Servidor iniciado em http://%s:&s", host,port)
+})
